181330ef862be2ee8d3c3525667f4f239254c6b7
Rafsun Jany Arman <armanrafsunjany@gmail.com>
2021-12-12 21:29:27 +0600

    feat: add new features with different UI but not removed previous codes

--- app/src/pages/components/MainContainer.tsx
+++ app/src/pages/components/MainContainer.tsx
@@ -4,7 +4,9 @@ import { ParsedData } from '../libs/types';
 import './MainContainer.scss';
 import * as download from '../../assets/images/download.svg';
 import * as add from '../../assets/images/add.svg';
+import * as logo from '../../assets/images/logo.png';
 import { PdfService } from '../libs/PdfService';
+import { IndexEvents } from '../libs/IndexEvents';
 
 export class MainContainer extends React.Component<
   {},
@@ -35,17 +37,6 @@ export class MainContainer extends React.Component<
     });
   }
 
-  reset() {
-    (document.getElementById('img-input') as HTMLInputElement).value = '';
-    var ele: any = document.getElementsByName('img-type');
-    for (var i = 0; i < ele.length; i++) ele[i].checked = false;
-    this.setState({
-      parsedData: [],
-      result: '',
-      images: [],
-    });
-  }
-
   private messageReceiver(parsedData: ParsedData) {
     const _ = this.state.parsedData;
     _.push(parsedData);
@@ -60,6 +51,11 @@ export class MainContainer extends React.Component<
     const file = event.target.files[0];
     var reader = new FileReader();
     reader.addEventListener('load', (_event: any) => {
+      Array.from(
+        document.getElementsByClassName(
+          'preview-properties'
+        ) as HTMLCollectionOf<HTMLElement>
+      )[0].style.backgroundImage = `url('${_event.target.result}')`;
       this.socketEvents?.sendToServer({
         type: 'image',
         data: _event.target.result.replace(/.*base64,/, ''),
@@ -82,95 +78,80 @@ export class MainContainer extends React.Component<
   render() {
     const acceptedType = '.png, .PNG, .jpg, .JPG, .JPEG, .jpeg';
     return (
-      <div className="main-container">
-        <div className="card-container">
-          <div className="card-1">
-            <label htmlFor="img-input">Please insert an image</label>
-            <input
-              type="file"
-              id="img-input"
-              accept={acceptedType}
-              onChange={this.imageInput.bind(this)}
-            />
-          </div>
-          <div className="card-2">
-            <label htmlFor="img-type">Select image type</label>
+      <div className="test-container">
+        <div className="preview-img preview-properties">
+          {/* <span id="previewText">
+            Select images to preview
+          </span> */}
+        </div>
+        <div className="main-container">
+          <div className="card-container">
+            <div className="card-1">
+              <label htmlFor="img-input">Please insert an image</label>
+              <input
+                type="file"
+                id="img-input"
+                accept={acceptedType}
+                onChange={this.imageInput.bind(this)}
+              />
+            </div>
+            <div className="card-2">
+              <label htmlFor="img-type">Select image type</label>
 
-            <div>
-              <input
-                type="radio"
-                id="wave_type"
-                name="img-type"
-                value="Wave"
-                onChange={this.radioBtn.bind(this)}
-              />
-              <label htmlFor="wave_type">Wave</label>
-              <input
-                type="radio"
-                id="spiral_type"
-                name="img-type"
-                value="Spiral"
-                onChange={this.radioBtn.bind(this)}
-              />
-              <label htmlFor="spiral_type">Spiral</label>
+              <div>
+                <input
+                  type="radio"
+                  id="wave_type"
+                  name="img-type"
+                  value="Wave"
+                  onChange={this.radioBtn.bind(this)}
+                />
+                <label htmlFor="wave_type">Wave</label>
+                <input
+                  type="radio"
+                  id="spiral_type"
+                  name="img-type"
+                  value="Spiral"
+                  onChange={this.radioBtn.bind(this)}
+                />
+                <label htmlFor="spiral_type">Spiral</label>
+              </div>
+            </div>
+            <div className="card-3">
+              <label>Status</label>
+              {this.state.parsedData.length
+                ? this.state.parsedData.map((data) => {
+                    if (data.type === 'status')
+                      return (
+                        <p key={data.value.tag} className="status-field">
+                          {data.value.tag}:{' '}
+                          <span className={data.status ? 'success' : 'error'}>
+                            {data.value.value}
+                          </span>
+                        </p>
+                      );
+                  })
+                : ''}
+            </div>
+            <div className="card-4">
+              <label>Results</label>
+              {this.state.parsedData.length
+                ? this.state.parsedData.map((data) => {
+                    if (data.type === 'results')
+                      return (
+                        <p key={data.value.tag} className="result-field">
+                          The handwriting is highly likely of a{' '}
+                          <span className={data.status ? 'success' : 'error'}>
+                            {data.value.value}
+                          </span>{' '}
+                          person!
+                        </p>
+                      );
+                  })
+                : ''}
             </div>
           </div>
-          <div className="card-3">
-            <label>Status</label>
-            {this.state.parsedData.length
-              ? this.state.parsedData.map((data) => {
-                  if (data.type === 'status')
-                    return (
-                      <p key={data.value.tag} className="status-field">
-                        {data.value.tag}:{' '}
-                        <span className={data.status ? 'success' : 'error'}>
-                          {data.value.value}
-                        </span>
-                      </p>
-                    );
-                })
-              : ''}
-          </div>
-          <div className="card-4">
-            <label>Results</label>
-            {this.state.parsedData.length
-              ? this.state.parsedData.map((data) => {
-                  if (data.type === 'results')
-                    return (
-                      <p key={data.value.tag} className="result-field">
-                        The handwriting is highly likely of a{' '}
-                        <span className={data.status ? 'success' : 'error'}>
-                          {data.value.value}
-                        </span>{' '}
-                        person!
-                      </p>
-                    );
-                })
-              : ''}
-          </div>
         </div>
-        {this.state.parsedData.length
-          ? this.state.parsedData.map((data) => {
-              if (data.type === 'ImageFile')
-                return (
-                  <div className="float-btn active">
-                    <img src={add.default} alt="add_logo" />
-                    {/* <img src={download.default} alt="download_logo" /> */}
-                  </div>
-                );
-              else
-                return (
-                  <div
-                    className="float-btn"
-                    // onClick={this.downloadPDF.bind(this)}
-                    onClick={this.reset.bind(this)}
-                  >
-                    <img src={add.default} alt="add_logo" />
-                    {/* <img src={download.default} alt="download_logo" /> */}
-                  </div>
-                );
-            })
-          : ''}
       </div>
     );
   }
--- app/src/pages/libs/IndexEvents.ts
+++ app/src/pages/libs/IndexEvents.ts
@@ -6,6 +6,9 @@ export class IndexEvents {
   private sidebar = document.getElementsByClassName('sidebar')[0];
   private aboutBtn = document.getElementById('aboutBtn');
   private aboutPopover = document.getElementsByClassName('about-container')[0];
+  private previewImage = document.getElementsByClassName('preview-img')[0];
+  private headerBG = document.getElementsByClassName('header-bg')[0];
+  private infoBtn = document.getElementById('infoBtn');
 
   private aboutSocialLinks = document.getElementsByClassName(
     'about-social__links'
@@ -14,6 +17,7 @@ export class IndexEvents {
   private backdropEventTarget: EventTarget | null = null;
 
   constructor() {
+    this.headerEvents();
     this.backdropEvents();
     this.sidebarBtnEvents();
     this.aboutPopoverEvents();
@@ -27,12 +31,13 @@ export class IndexEvents {
   }
 
   private sidebarBtnEvents() {
-    this.menuBtn?.addEventListener('click', (event: Event) => {
+    // this.menuBtn?.addEventListener('click', (event: Event) => {
+    //   this.backdrop?.classList.add('backdrop--active');
+    //   this.sidebar.classList.add('sidebar--active');
+    // });
+    this.infoBtn?.addEventListener('click', () => {
+      // this.sidebar!.classList.remove('sidebar--active');
       this.backdrop?.classList.add('backdrop--active');
-      this.sidebar.classList.add('sidebar--active');
-    });
-    this.aboutBtn?.addEventListener('click', () => {
-      this.sidebar!.classList.remove('sidebar--active');
       this.aboutPopover.classList.add('about-container--active');
     });
   }
@@ -50,6 +55,20 @@ export class IndexEvents {
         window.open(linksParser[i]);
       });
     }
+  }
+
+  private headerEvents() {
+    document.addEventListener('scroll', () => {
+      const menuBtnHeight = this.menuBtn?.getBoundingClientRect().height;
+      if (
+        menuBtnHeight &&
+        this.previewImage.getBoundingClientRect().top < menuBtnHeight
+      ) {
+        this.headerBG?.classList.add('header-bg--active');
+      } else {
+        this.headerBG.classList.remove('header-bg--active');
+      }
+    });
   }
 
   private dispatchEvents() {
